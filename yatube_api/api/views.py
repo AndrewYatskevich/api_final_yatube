4484e64d5230c5daca807a700edb3d8841db2df3
Andrew Yatskevich <drew.yatskevich@gmail.com>
2022-07-27 16:34:01 +0300

    complete sprint 8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,87 @@
-# TODO:  Напишите свой вариант
+from django.db import IntegrityError
+
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import SearchFilter
+
+from posts.models import Post, Group, Comment, Follow, User
+
+from .serializers import (PostSerializer, GroupSerializer, CommentSerializer,
+                          FollowSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_destroy(instance)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        queryset = Comment.objects.filter(post_id=post_id)
+        return queryset
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_destroy(instance)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post_id=self.kwargs.get('post_id')
+        )
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated, )
+    filter_backends = (SearchFilter, )
+    search_fields = ('following__username', )
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        queryset = Follow.objects.filter(user=user_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        username = self.request.POST.get('following')
+        try:
+            serializer.save(
+                user=self.request.user,
+                following=User.objects.get(username=username)
+            )
+        except User.DoesNotExist:
+            raise ValidationError('Неверное имя пользователя')
+        except IntegrityError:
+            raise ValidationError('Нельзя подписаться на себя')
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,11 +4,33 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField('Заголовок', max_length=200)
+    slug = models.SlugField('Ссылка', unique=True)
+    description = models.TextField('Описание')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name=u'Группа',
+        help_text='Выберите группу',
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
 
@@ -24,3 +46,34 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following_author'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'following'),
+                name='unique_subscription'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('following')),
+                name='own_subscription'
+            ),
+        ]
+
+    def __str__(self):
+        return (f'Пользователь {self.user.username}'
+                f' подписан на пользователя {self.following.username}')
